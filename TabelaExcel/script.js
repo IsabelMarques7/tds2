ae898421fd58ea40e6ab9965e6336b4aa6c6f08b
IsabelMarques7 <isabelmarquesdossantos7@gmail.com>
2024-03-06 17:07:18 -0300

    aprendi coisinhas

--- TabelaExcel/script.js
+++ TabelaExcel/script.js
@@ -1,38 +1,35 @@
 function adicionarItem(){
     // obter os valores dos campos de entrada
-    var nome = document.getElementById("nome").value;
+    var nome = document.getElementById("nome").value; 
     var valor = document.getElementById("valor").value;
-    var quantidade= document.getElementById("quantidade").value;
+    var quantidade = document.getElementById("quantidade").value;
 
     // validar se os campos estão preenchidos
     if(!nome || !valor || !quantidade){
-        alert("preencha todos os campos")
+        alert("Preencha todos os campos");
         return;
     }
 
-    // usada para imprimir mensagens no console do navegador
-    // console.log(nome, valor, quantidade)
-
-    // criar linha na tabela com 3 celulas
+    // criar uma linha na tabela com 3 celulas
     var tabela = document.getElementById("tabela").getElementsByTagName("tbody")[0];
-    var novaLinha = tabela.insertRow(tabela.rows,length);
+    var novaLinha = tabela.insertRow(tabela.rows.length);
 
-    var celulaNome = novaLinha.insertCell(0)
-    var celulaValor = novaLinha.insertCell(1)
-    var celulaQuantidade = novaLinha.insertCell(2)
+    var celulaNome = novaLinha.insertCell(0);
+    var celulaValor = novaLinha.insertCell(1);
+    var celulaQuantidade = novaLinha.insertCell(2);
 
-    // atribuir as celulas os valores digitados
+    //Atribuir as celulas os valores digitados.
     celulaNome.innerHTML = nome;
     celulaValor.innerHTML = valor;
     celulaQuantidade.innerHTML = quantidade;
 
-    //limpar os campos
-    document.getElementById("nome").value = ""
-    document.getElementById("valor").value = ""
-    document.getElementById("quantidade").value = ""
+    // limpar os campos
+    document.getElementById("nome").value = "";
+    document.getElementById("valor").value = "";
+    document.getElementById("quantidade").value = "";
 }
 
-function exportParaExcel(){
+function ExportarParaExcel(){
     var tabela = document.getElementById("tabela")
     var nomeArquivo = "tabela_produtos.xlsx"
     var wb = XLSX.utils.table_to_book(tabela, {sheet: "tabela de produtos "})
